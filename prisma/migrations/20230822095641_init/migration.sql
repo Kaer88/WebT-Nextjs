312506c435fe5d0dc7a99454e08ff4e6ddeeb686
Kaer88 <robert.koncz88@gmail.com>
2023-08-22 12:42:04 +0200

    update db

--- prisma/migrations/20230822095641_init/migration.sql
+++ prisma/migrations/20230822095641_init/migration.sql
@@ -1,9 +1,3 @@
-/*
-  Warnings:
-
-  - Added the required column `amount` to the `Cart` table without a default value. This is not possible if the table is not empty.
-
-*/
 -- RedefineTables
 PRAGMA foreign_keys=OFF;
 CREATE TABLE "new_Cart" (
@@ -12,8 +6,9 @@ CREATE TABLE "new_Cart" (
     "amount" INTEGER NOT NULL,
     CONSTRAINT "Cart_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Products" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
-INSERT INTO "new_Cart" ("id", "productId") SELECT "id", "productId" FROM "Cart";
+INSERT INTO "new_Cart" ("amount", "id", "productId") SELECT "amount", "id", "productId" FROM "Cart";
 DROP TABLE "Cart";
 ALTER TABLE "new_Cart" RENAME TO "Cart";
+CREATE UNIQUE INDEX "Cart_productId_key" ON "Cart"("productId");
 PRAGMA foreign_key_check;
 PRAGMA foreign_keys=ON;
