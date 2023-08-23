061782fbca52e129c2bfcc68370d054a47f69571
Kaer88 <robert.koncz88@gmail.com>
2023-08-23 11:17:50 +0200

    fix button

--- src/components/cart/CartItem.jsx
+++ src/components/cart/CartItem.jsx
@@ -25,7 +25,7 @@ export default function CartItem({ item }) {
             <span>{item.name}</span>
             <span>{item.amount} db</span>
             <span>{item.price} Ft</span>
-            <button onClick={mutate} className="outline outline-slate-700 p-1">{isLoading ? "..." : "Remove Item"}</button>
+            <button onClick={mutate} disabled={isLoading} className="outline outline-slate-700 p-1">{"Remove Item"}</button>
             <div>
                 <span>x </span>
                 <input className="w-10" type="number" onChange={(e) => setInputState(e.target.value)} value={inputState} />
